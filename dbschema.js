222c33b4dcff6865297689e4c5208f8eebd37571
Ross Hazelett <rhhazelett@gmail.com>
2020-04-30 23:57:32 -0400

    everything working

---
+++ dbschema.js
@@ -0,0 +1,9 @@
+let db = {
+  woofs: [
+    {
+      userHandle: 'user',
+      body: 'this is the woof body',
+      createdAt: '2020-04-29T22:02:14.359Z'
+    }
+  ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,65 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const cors = require("cors");
 
 admin.initializeApp();
 
+const express = require('express');
+const app = express();
 
-exports.getWoofs = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('woofs').get()
+app.use(cors({ origin: true }));
+
+
+//////////
+// GET // Woofs
+//////////////
+app.get('/woofs', (req, res) => {
+  admin
+    .firestore()
+    .collection('woofs')
+    .orderBy('createdAt', 'desc')
+    .get()
     .then((data) => {
       let woofs = [];
       data.forEach((doc) => {
-        woofs.push(doc.data());
+        woofs.push({
+          woofId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
-      return res.json(woofs)
+      return res.json(woofs);
     })
     .catch((err) => console.error(err));
 });
 
-exports.createWoof = functions.https.onRequest((req, res) => {
+///////////
+// POST // a woof
+////////////////
+app.post('/woof', (req, res) => {
   const newWoof = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
-
   admin.firestore()
     .collection('woofs')
     .add(newWoof)
     .then((doc) => {
-      res.json({ message: `document ${doc.id} created successfully` });
+      let responseFromPost = {
+        message: `document ${doc.id} created successfully`,
+        statusMessage: "success",
+        createdWoofId: doc.id
+      }
+      res.json(responseFromPost)
     })
     .catch((err) => {
       res.status(500).json({ error: 'something went wrong' });
       console.error(err);
-    })
-
+    });
 });
+
+
+
+exports.api = functions.https.onRequest(app);
